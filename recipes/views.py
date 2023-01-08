c73ce7506ba69d34a7f7941a00759fb6de583919
Pedro Fernandes <pedrofernandesalves2003@gmail.com>
2023-01-08 18:32:28 -0300

    reconfiguração e organização dos templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from utils.recipes.factory import make_fake
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_fake() for _ in range(10)],
+    })
 
 def receita(request, id):
-    return render(request, 'recipes/pages/receita-view.html')
+    return render(request, 'recipes/pages/receita-view.html', context= {
+        'recipe': make_fake()
+    })
 
